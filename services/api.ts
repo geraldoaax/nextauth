441bdee2fac7f34392830891f765aa740038a15c
Geraldo Dutra Neto <geraldoaax@gmail.com>
2022-03-27 19:04:32 -0300

    refresh token error

--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
 
 let cookies = parseCookies()
+let isRefreshing = false
+let failedRequestQueue = []
 
 export const api = axios.create(
   {
@@ -20,26 +22,46 @@ api.interceptors.response.use(response => {
       cookies = parseCookies()
 
       const { 'nextauth.refreshtoken': refreshtoken } = cookies
+      const originalConfig = error.config
 
-      api.post('/refresh', {
-        refreshtoken,
-      }).then(response => {
-        const { token } = response.data
+      if (!isRefreshing) {
+        api.post('/refresh', {
+          refreshtoken,
+        }).then(response => {
+          const { token } = response.data
 
+          setCookie(undefined, 'nextauth.token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 dias
+            path: '/'
+          })
 
+          setCookie(undefined, 'nextauth.refreshtoken', response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30, // 30 dias
+            path: '/'
+          })
 
-        setCookie(undefined, 'nextauth.token', token, {
-          maxAge: 60 * 60 * 24 * 30, // 30 dias
-          path: '/'
+          api.defaults.headers['Authorization'] = `Bearer ${token}`
+          failedRequestQueue.forEach(request => request.onSuccess(token))
+          failedRequestQueue = []
+        }).catch(err => {
+          failedRequestQueue.forEach(request => request.onFailure(err))
+          failedRequestQueue = []
+        }).finally(() => {
+          isRefreshing = false
         })
-        setCookie(undefined, 'nextauth.refreshtoken', response.data.refreshToken, {
-          maxAge: 60 * 60 * 24 * 30, // 30 dias
-          path: '/'
+      }
+
+      return new Promise((resolve, reject) => { //unica forma do axios retornar uma promise para aguardar q seja executado
+        failedRequestQueue.push({
+          onSuccess: (token: string) => {
+            originalConfig.headers['Authorization'] = `Bearer ${token}`
+
+            resolve(api(originalConfig))
+          },
+          onFailure: (err: AxiosError) => {
+            reject(err)
+          }
         })
-        
-        api.defaults.headers['Authorization'] = `Bearer ${token}`
-
-
       })
     } else {
       //deslogar user
