e8ca7d18e08950db62937d66c333acbef1aa76fd
Geraldo Dutra Neto <geraldoaax@gmail.com>
2022-03-27 15:25:06 -0300

    dashboard

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,12 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+import Router from "next/router";
 import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string;
+  roles: string[];
+}
 
 type SignInCredentials = {
   email: string;
@@ -8,28 +15,43 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
+  isAuthenticated: boolean;
 };
 
 type AuthProviderProps = {
   children: ReactNode;
-
 }
 
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>()
+  const isAuthenticated = !!user
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post('sessions', { email, password })
 
-      console.log(response.data)
+      const { token, refreshToken, permissions, roles } = response.data
+
+      //localstorage --fica disponivel em outras sessoes, no next fica no sserver e precisa adaptar
+      //sessionStorage --nao fica disponivel em outras sessoes
+      //cookies -- pode ser acessado dos dois lados
+
+
+      setUser({
+        email,
+        permissions,
+        roles
+      })
+
+      Router.push('/dashboard')
     } catch (err) {
       console.log(err)
 
     }
   }
 
-  return <AuthContext.Provider value={{ signIn, isAuthenticated }}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>{children}</AuthContext.Provider>;
 }
