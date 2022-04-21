d631c6f3b8bcbd60d0218c399468910ceb79bde0
Geraldo Dutra Neto <geraldoaax@gmail.com>
2022-04-21 17:54:58 -0300

    broadcast

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -16,6 +16,8 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut: () => void;
+
   user: User;
   isAuthenticated: boolean;
 };
@@ -26,9 +28,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+let authChannel: BroadcastChannel
+
 export function signOut() {
   destroyCookie(undefined, 'nextauth.token')
   destroyCookie(undefined, 'nextauth.refreshToken')
+
+  authChannel.postMessage('signOut')
 
   typeof window !== 'undefined' && Router.push('/')
   // Router.push('/')
@@ -37,6 +43,23 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>()
   const isAuthenticated = !!user
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel('auth')
+
+    authChannel.onmessage = (message) => {
+      switch (message.data) {
+        case 'signOut':
+          signOut()
+          break
+        case 'signIn':
+          Router.push('/dashboard')
+          break
+        default:
+          break
+      }
+    }
+  })
 
   useEffect(() => {
     const { 'nextauth.token': token } = parseCookies()
@@ -84,11 +107,13 @@ export function AuthProvider({ children }: AuthProviderProps) {
       typeof window !== 'undefined' && Router.push('/dashboard')
       // Router.push('/dashboard')
 
+      authChannel.postMessage('signIn')
+
     } catch (err) {
       console.log(err)
 
     }
   }
 
-  return <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user }}>{children}</AuthContext.Provider>;
 }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,14 +1,15 @@
+import { Button } from '@chakra-ui/react';
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
-import { useCan } from "../hooks/useCan";
-import { Can } from "../components/Can";
+
 
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext)
+  const { user, signOut, isAuthenticated } = useContext(AuthContext)
 
 
 
@@ -23,6 +24,9 @@ export default function Dashboard() {
     <>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <h1>Dashboard: {user?.email}</h1>
+
+        <Button colorScheme='teal' variant='outline' onClick={signOut}>Sign Out</Button>
+
         <Can permissions={['metrics.list']}>
           <div>Métricas</div>
         </Can>
